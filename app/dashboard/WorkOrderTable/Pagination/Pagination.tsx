e04c399fb6b11b449925eacf7c22c9fdbb2317bd
ShriduttPatelBacancy <shridutt.patel@bacancy.com>
2024-04-10 18:24:25 +0530

    integrated custom pagination methods

--- app/dashboard/WorkOrderTable/Pagination/Pagination.tsx
+++ app/dashboard/WorkOrderTable/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import PaginationBtn from '@/app/dashboard/WorkOrderTable/Pagination/PaginationBtn';
 
@@ -7,10 +7,66 @@ interface WorkOrderPaginationProps {
   totalPages: number;
   onPageChange: (pageIndex: number) => void;
   activeClass?: string;
+  maxPaginationSize?: number;
 }
 
-const Pagination = ({ currentPage, totalPages, onPageChange, activeClass }: WorkOrderPaginationProps) => {
+const Pagination = ({ currentPage, totalPages, onPageChange, activeClass, maxPaginationSize = 7 }: WorkOrderPaginationProps) => {
+  if(maxPaginationSize % 2  === 0) throw new Error('maxPaginationSize should be odd number')
   // Calculate start and end buttons based on current page and total pages
+  const paginationArr: Array<{label: string, page: number}> = Array.from({ length: maxPaginationSize });
+  
+  if(maxPaginationSize + 1 >= totalPages) {
+    for(let i = 0; i < totalPages; i++) {
+      paginationArr[i] = { label: `${i}`, page: i };
+    }
+  } else {
+    paginationArr[0] = { label: '1', page: 1 };
+    paginationArr[paginationArr.length - 1] = { label: `${totalPages}`, page: totalPages };
+  
+    const leftPivotPoint = maxPaginationSize - 2;
+    if(currentPage < leftPivotPoint) {
+      for(let i = 1; i <= maxPaginationSize - 3; i++) {
+        paginationArr[i] = { label: `${i + 1}`, page: i + 1 };
+      }
+      paginationArr[paginationArr.length - 2] = { label: '...', page: Math.floor(totalPages / 2) };
+    } else if(leftPivotPoint <= (currentPage + 1)) {
+      paginationArr[1] = { label: '...', page: Math.ceil(currentPage / 2) };
+      let pointer = Math.floor((maxPaginationSize - 4) / 2) * (-1); 
+      for(let i = 2; i < maxPaginationSize - 2; i++) {
+         // 7 - 4 (7) ==> 3/2 floor - 1
+         // 8 - 4 (4) ==> 
+         const num = (currentPage + 1) + pointer;
+         paginationArr[i] = { label: `${num}`, page: num }
+         pointer++;
+      }
+      paginationArr[paginationArr.length - 2] = { label: '...', page: Math.floor(totalPages / 2) };
+  
+    } else if(totalPages - (currentPage + 1) <= 5) {
+      paginationArr[1] = { label: '...', page: Math.ceil(currentPage / 2) };
+      const startPoint = Math.min(maxPaginationSize)
+      for(let i = 2; i < maxPaginationSize - 1; i++) {
+        paginationArr[i] = { label: `${currentPage + i - 1}`, page: currentPage + i - 1 };
+      }
+    }
+  }
+
+  return (
+    <>
+      {
+        paginationArr.map((pageInfo, index) => (
+          <PaginationBtn
+            key={index}
+            isActive={pageInfo.label !== '...' && pageInfo.page - 1 === currentPage}
+            onClick={() => onPageChange(pageInfo.page - 1)}
+            activeClass={activeClass}
+          >
+            {pageInfo.label}
+          </PaginationBtn>
+        ))
+      }
+    </>
+  )
+
   let startPage =    - 3;
   let endPage = currentPage + 3;
 
